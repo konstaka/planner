eaea6a633342715d477ae4c1795cc8976db3283e
Konsta Kanniainen <32358531+konstaka@users.noreply.github.com>
2020-04-10 16:46:41 +0200

    WW data and arte/ww filtering

--- src/main/java/planner/TargetVillage.java
+++ src/main/java/planner/TargetVillage.java
@@ -1,5 +1,8 @@
 package planner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,6 +15,18 @@ public class TargetVillage extends Village {
     @Getter
     @Setter
     private boolean offvillage = false;
+
+    @Getter
+    @Setter
+    private boolean wwvillage = false;
+
+    @Getter
+    @Setter
+    private String artefact = "";
+
+    @Getter
+    @Setter
+    private List<String> arteEffects = new ArrayList<>();
 
     public TargetVillage(int coordId) {
         super(coordId);
